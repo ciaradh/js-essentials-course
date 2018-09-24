193a60b84df3f1b18d3027bd68c4f7653c768eaa
Monica Pedron <monicapedron23@gmail.com>
2018-09-24 21:31:05 +0200

    Update server.js to make chat work

--- client/app.js
+++ client/app.js
@@ -1,1 +1,16 @@
-alert('hello from the JS file');
+var socket = io();
+
+$('form').submit(function () {
+  var text = $('#message').val();
+  var initials = $('#initials').val();
+    
+  socket.emit('message', text);
+  socket.emit('initials', initials)    
+  $('#message').val('');
+  $('#initials').val('');
+  return false;
+});
+
+socket.on('message', function (msg) {
+  $('<li class="Chat__historyItem">').text(msg).appendTo('#history');
+});
